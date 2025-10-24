0c72de27142aa691f9490e68c1b7af507a6e32a0
owen <persimmonautomation@gmail.com>
2025-10-23 20:11:07 -0400

    refactor: Update ui/index.tsx

--- src/components/ui/index.tsx
+++ src/components/ui/index.tsx
@@ -3,6 +3,4 @@
  */
 
 export { ConfirmDialog } from './ConfirmDialog'
-export { FileSizeWarningDialog } from './FileSizeWarningDialog'
-export { UploadFolderDialog } from './UploadFolderDialog'
 export { useToast } from './use-toast'
